557e48e59bc2ebec897b14be9c610bc760d1a887
anisa kazanbaeva <anisahime@yandex.ru>
2024-11-09 23:51:59 +1000

    add fixtures for tests

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+
+from src.category import Category
+from src.product import Product
+
+
+@pytest.fixture
+def some_product():
+    return Product("Пояс", "Тканевый, с крепкой пряжкой", 1500.0, 11)
+
+
+@pytest.fixture
+def first_category():
+    category_1 = Category(
+        "Украшения",
+        "Любовь каждой женщины",
+        [
+            Product("Ожерелье", "Красивое ожерелье из морского жемчуга", 10000.0, 2),
+            Product("Браслет", "Изящный пластинчатый браслет из натуральных камней", 3500.0, 3),
+            Product("Кольцо", "Серебряное кольцо с сияющим камнем", 2500.0, 5),
+        ],
+    )
+    return category_1
+
+
+@pytest.fixture
+def second_category():
+    category_2 = Category(
+        "Аксессуары",
+        "Подойдут для любого наряда",
+        [
+            Product("Сумка", "Сумка из натуральной кожи", 7000.0, 1),
+            Product("Пояс", "Тканевый, с крепкой пряжкой", 1500.0, 11),
+        ],
+    )
+    return category_2
